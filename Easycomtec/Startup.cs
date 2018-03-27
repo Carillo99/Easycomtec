6e8e8af42819e9021a3b786b4ddad025df7591ec
Carillo99 <gleissoncarillo@hotmail.com>
2018-03-27 17:38:47 -0300

    Adicionar arquivos de projeto.

---
+++ Easycomtec/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Easycomtec.Startup))]
+namespace Easycomtec
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
